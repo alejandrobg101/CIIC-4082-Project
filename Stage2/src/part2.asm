0df51967f9dbe1358a9fca351ee8ebe7eceb83ed
Alejandro Behar <alejandro.behar@upr.edu>
2024-04-01 23:07:20 -0400

    LAst changes stage 2

--- Stage2/src/part2.asm
+++ Stage2/src/part2.asm
@@ -22,8 +22,10 @@ walk_frame_counter : .res 2 ; Counter for walking animation frames
   LDA #$02
   STA OAMDMA
   LDA #$00
-  
+
+	; JSR update_player
   JSR draw_player
+
 
   STA $2005
   STA $2005
@@ -120,95 +122,144 @@ forever:
   JMP forever
 .endproc
 
-.proc draw_player
-    ; Draw standing player tiles
-    ; Write player tile numbers for standing
-    LDA #$02  ; Adjust these values according to your specific tile mappings
-    STA $0201
-    LDA #$03
-    STA $0205
-    LDA #$12
-    STA $0209
-    LDA #$13
-    STA $020d
-    JMP DrawDone
+; .proc update_player
+;   PHP
+;   PHA
+;   TXA
+;   PHA
+;   TYA
+;   PHA
 
-    ;   ; write player tile attributes
-    ; use palette 0
-    LDA #$00
-    STA $0202
-    STA $0206
-    STA $020a
-    STA $020e
+;   LDA player_x
+;   CMP #$e0
+;   BCC not_at_right_edge
+;   ; if BCC is not taken, we are greater than $e0
+;   LDA #$00
+;   STA player_dir    ; start moving left
+;   JMP direction_set ; we already chose a direction,
+;                     ; so we can skip the left side check
+; not_at_right_edge:
+;   LDA player_x
+;   CMP #$10
+;   BCS direction_set
+;   ; if BCS not taken, we are less than $10
+;   LDA #$01
+;   STA player_dir   ; start moving right
+; direction_set:
+;   ; now, actually update player_x
+;   LDA player_dir
+;   CMP #$01
+;   BEQ move_right
+;   ; if player_dir minus $01 is not zero,
+;   ; that means player_dir was $00 and
+;   ; we need to move left
+;   DEC player_x
+;   JMP exit_subroutine
+; move_right:
+;   INC player_x
+
+; exit_subroutine:
+;   ; all done, clean up and return
+;   PLA
+;   TAY
+;   PLA
+;   TAX
+;   PLA
+;   PLP
+;   RTS
+; .endproc
+
+.proc draw_player
+  ; save registers
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; Use a flag to alternate between sprite sets
+  LDA walk_frame_counter
+  AND #$01  ; Keep only the least significant bit to alternate between 0 and 1
+  BEQ draw_sprite_set1
+  JMP draw_sprite_set2
+
+draw_sprite_set1:
+  ; write player ship tile numbers for sprite set 1
+  LDA #$02
+  STA $0201
+  LDA #$03
+  STA $0205
+  LDA #$12
+  STA $0209
+  LDA #$13
+  STA $020d
+  JMP sprite_attributes_set1
+
+draw_sprite_set2:
+  ; write player ship tile numbers for sprite set 2
+  LDA #$04
+  STA $0201
+  LDA #$05
+  STA $0205
+  LDA #$14
+  STA $0209
+  LDA #$15
+  STA $020d
+
+sprite_attributes_set1:
+  ; write player ship tile attributes for both sets
+  ; use palette 0
+  LDA #$00
+  STA $0202
+  STA $0206
+  STA $020a
+  STA $020e
+
+  ; store tile locations
+  ; top left tile:
+  LDA player_y
+  STA $0200
+  LDA player_x
+  STA $0203
+
+  ; top right tile (x + 8):
+  LDA player_y
+  STA $0204
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $0207
+
+  ; bottom left tile (y + 8):
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $0208
+  LDA player_x
+  STA $020b
+
+  ; bottom right tile (x + 8, y + 8)
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $020c
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $020f
+  INC walk_frame_counter
+  ; restore registers and return
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
 .endproc
 
 
-.proc WalkingLeft
-    ; Determine which frame of walking animation to draw
-    ; Use a counter or timer to alternate between frames
-
-    ; Increment walk_frame_counter
-    INC walk_frame_counter
-
-    ; Call the appropriate subroutine for the current frame of animation
-    ; This example assumes there are three frames for walking left
-    LDA walk_frame_counter
-    AND #$03     ; Mask to ensure the counter stays within the range of 0 to 2
-    CMP #$00
-    BEQ DrawLeftFrame1
-    CMP #$01
-    BEQ DrawLeftFrame2
-    ; Default to the third frame if the counter is 2
-    JMP DrawLeftFrame3
-
-DrawLeftFrame1:
-    ; Draw left-facing player tiles for the first frame of walking animation
-    ; Write player tile numbers for the first frame
-    LDA #$24  ; Adjust these values according to your specific tile mappings
-    STA $0201
-    LDA #$25
-    STA $0205
-    LDA #$34
-    STA $0209
-    LDA #$35
-    STA $020d
-    JMP DrawDone
-
-DrawLeftFrame2:
-    ; Draw left-facing player tiles for the second frame of walking animation
-    ; Write player tile numbers for the second frame
-    LDA #$22  ; Adjust these values according to your specific tile mappings
-    STA $0201
-    LDA #$23
-    STA $0205
-    LDA #$32
-    STA $0209
-    LDA #$33
-    STA $020d
-    JMP DrawDone
-
-DrawLeftFrame3:
-    ; Draw left-facing player tiles for the third frame of walking animation
-    ; Write player tile numbers for the third frame
-    LDA #$0f  ; Adjust these values according to your specific tile mappings
-    STA $0201
-    LDA #$0e
-    STA $0205
-    LDA #$1f
-    STA $0209
-    LDA #$1e
-    STA $020d
-    JMP DrawDone
-
-DrawDone:
-    ; Restore registers and return
-    PLA
-    TAY
-    PLA
-    TAX
-    PLA
-    PLP
-    RTS
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
@@ -226,66 +277,7 @@ palettes:
 .byte $0f, $19, $09, $29
 
 sprites:
-; ; ;front 1
-; .byte $60, $02, $00, $50
-; .byte $60, $03, $00, $58
-; .byte $68, $12, $00, $50
-; .byte $68, $13, $00, $58
-; ;front2
-; .byte $60, $04, $00, $60
-; .byte $60, $05, $00, $68
-; .byte $68, $14, $00, $60
-; .byte $68, $15, $00, $68
-; ;front3
-; .byte $60, $06, $00, $70
-; .byte $60, $07, $00, $78
-; .byte $68, $16, $00, $70
-; .byte $68, $17, $00, $78
-; ;back1
-; .byte $70, $08, $00, $50
-; .byte $70, $09, $00, $58
-; .byte $78, $18, $00, $50
-; .byte $78, $19, $00, $58
-; ;back2
-; .byte $70, $0a, $00, $60
-; .byte $70, $0b, $00, $68
-; .byte $78, $1a, $00, $60
-; .byte $78, $1b, $00, $68
-; ;back3
-; .byte $70, $0c, $00, $70
-; .byte $70, $0d, $00, $78
-; .byte $78, $1c, $00, $70
-; .byte $78, $1d, $00, $78
-; ;left1
-; .byte $80, $0e, $00, $50
-; .byte $80, $0f, $00, $58
-; .byte $88, $1e, $00, $50
-; .byte $88, $1f, $00, $58
-; ;left2
-; .byte $80, $22, $00, $60
-; .byte $80, $23, $00, $68
-; .byte $88, $32, $00, $60
-; .byte $88, $33, $00, $68
-; ;left3
-; .byte $80, $24, $00, $70
-; .byte $80, $25, $00, $78
-; .byte $88, $34, $00, $70
-; .byte $88, $35, $00, $78
-; ;right1
-; .byte $90, $26, $00, $50
-; .byte $90, $27, $00, $58
-; .byte $98, $36, $00, $50
-; .byte $98, $37, $00, $58
-; ;right2
-; .byte $90, $28, $00, $60
-; .byte $90, $29, $00, $68
-; .byte $98, $38, $00, $60
-; .byte $98, $39, $00, $68
-; ;right3
-; .byte $90, $2a, $00, $70
-; .byte $90, $2b, $00, $78
-; .byte $98, $3a, $00, $70
-; .byte $98, $3b, $00, $78
+
 
 .segment "CHR"
 .incbin "graphics.chr"
